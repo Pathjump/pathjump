ed7f8fb06c98c3857acb77c2401b54a55ca45df7
ahmed <a.ibrahim@objects.ws>
2013-11-04 11:48:58 +0200

    login social icons

--- web/js/main_new.js
+++ web/js/main_new.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
+  if($('.bxslider').length > 0){
   $('.bxslider').bxSlider({responsive: true, useCSS: false,mode :'fade',pager:false ,controls:false ,captions:false,auto:true});
+  }
+  if($('#myCarouse3').length > 0){
     $('#myCarouse3').carousel({
     interval: 2000
-    })
-
+    });
+  }
 });
